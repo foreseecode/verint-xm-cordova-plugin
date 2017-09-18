dd393ecdee57f5365efc548d348ae82960e15a9b
PM <pavel.mogilevsky@gmail.com>
2017-09-18 13:32:28 -0700

    init removed indeed

--- www/ForeSeeAPI.js
+++ www/ForeSeeAPI.js
@@ -1,15 +1,8 @@
 var exec = require('cordova/exec');
 
-
-exports.init = function(arg0, sucess, error){
-    exec(sucess, error, "ForeSeeAPI", "init", [arg0]);
-};
-
-
 exports.showSurvey = function(arg0, sucess, error){
     exec(sucess, error, "ForeSeeAPI", "showSurvey", [arg0]);
 };
-
 
 exports.showInvite = function(arg0, sucess, error){
     exec(sucess, error, "ForeSeeAPI", "showInvite", [arg0]);
