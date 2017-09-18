741b17ceecad24a7b428decd6930c5715116c854
PM <pavel.mogilevsky@gmail.com>
2017-09-18 13:00:51 -0700

    1. removed init action, 2.removed debug logs

--- src/android/ForeSeeAPI.java
+++ src/android/ForeSeeAPI.java
@@ -23,10 +23,8 @@ public class ForeSeeAPI extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         Log.d(sTag, "Action " + action);
-        if(action.equals("init")){
-            this.init(callbackContext);
-            return true;
-        }else if(action.equals("showSurvey")){
+
+        if(action.equals("showSurvey")){
             this.showSurvey(args.getString(0), callbackContext);
             return true;
         }else if(action.equals("showInvite")){
@@ -37,17 +35,6 @@ public class ForeSeeAPI extends CordovaPlugin {
             return true;
         }
         return false;
-    }
-
-    private void init(CallbackContext callbackContext){
-        try {
-            Log.d(sTag, "executing the init phase");
-            ForeSee.start(cordova.getActivity().getApplication());
-            callbackContext.success();
-        }catch (Exception ex){
-            Log.d(sTag, ex.getMessage());
-            callbackContext.error(sTag + " init phase failure");
-        }
     }
 
     private void showSurvey(final String id, final  CallbackContext callback){
@@ -68,12 +55,10 @@ public class ForeSeeAPI extends CordovaPlugin {
         try {
             cordova.getActivity().runOnUiThread(new Runnable(){
                 public void run(){
-                    Log.d(sTag, "Show invite for survey " + id);
                     ForeSee.showInviteForSurveyID(id);
                     callback.success();
                 }
             });
-            
         }catch (Exception ex){
             Log.d(sTag, ex.getMessage());
             callback.error(sTag + " show invite failure");
@@ -81,22 +66,7 @@ public class ForeSeeAPI extends CordovaPlugin {
     }
 
     private void checkEligibility(CallbackContext callback){
-        Log.d(sTag, "checkEligibility for survey");
         ForeSee.checkIfEligibleForSurvey();
         callback.success();
-    }
-
-     @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(sTag, "onStart for cordova");
-        ForeSee.start(cordova.getActivity().getApplication());
-    }
-
-
-    @Override
-    public void onResume(boolean multitasking) {
-        super.onResume(multitasking);
-        Log.d(sTag, "onResume");
     }
 }
