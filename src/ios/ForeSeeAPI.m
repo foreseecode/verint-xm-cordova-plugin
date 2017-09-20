897a667ffde46ced0af26d7c52471fb676b7ed59
PM <pavel.mogilevsky@gmail.com>
2017-09-20 12:17:14 -0700

    added methods for iOS API for JS

--- src/ios/ForeSeeAPI.m
+++ src/ios/ForeSeeAPI.m
@@ -6,32 +6,67 @@
   // Member variables go here.
 }
 
-- (void)checkEligibility:(CDVInvokedUrlCommand*)command;
+// methods exposed to JS as API
+- (void)showSurvey: (CDVInvokedUrlCommand*)command;
 
-- (void)showSurvey:(CDVInvokedUrlCommand*)command;
+- (void)showInivite: (CDVInvokedUrlCommand*)command;
 
-- (void)showInivite:(CDVInvokedUrlCommand*)command;
+- (void)checkEligibility: (CDVInvokedUrlCommand*)command;
+
+- (void)addCPPValue: (CDVInvokedUrlCommand*)command;
+
+- (void)removeCPPValue: (CDVInvokedUrlCommand*)command;
+
+- (void)incrementPageViewsCount: (CDVInvokedUrlCommand*)command;
+
+- (void)incrementSignificantEventCount: (CDVInvokedUrlCommand*)command;
+
+- (void)resetState: (CDVInvokedUrlCommand*)command;
 
 @end
 
 @implementation ForeSeeAPI
 
-- (void)checkEligibility:(CDVInvokedUrlCommand*)command
+
+
+- (void)checkEligibility: (CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 
     [ForeSee checkIfEligibleForSurvey];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void)showSurvey:(CDVInvokedUrlCommand*)command
+- (void)showSurvey: (CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* surveyId = [command.arguments objectAtIndex:0];
 
     if (surveyId != nil && [surveyId length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+
+        [ForeSee showSurveyForSurveyID:surveyId];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+- (void)showInvite: (CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* surveyId = [command.arguments objectAtIndex:0];
+
+    if (surveyId != nil && [surveyId length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+
+        [ForeSee show]
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
@@ -40,13 +75,15 @@
 }
 
 
-- (void)showInvite:(CDVInvokedUrlCommand*)command
+- (void)addCPPValue: (CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* surveyId = [command.arguments objectAtIndex:0];
+    NSString* key = [command.arguments objectAtIndex:0];
+    NSString* value = [command.arguments objectAtIndex:1];
 
-    if (surveyId != nil && [surveyId length] > 0) {
+     if (key != nil && [key length] > 0 && value != nil && [value length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        [ForeSee addCPPValue:key forKey:key];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
@@ -54,5 +91,58 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+-(void)removeCPPValue: (CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* key = [command.arguments objectAtIndex:0];
+
+    if (key != nil && [key length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        [ForeSee removeCPPValueForKey:key];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void)incrementPageViewsCount: (CDVInvokedUrlCommand*)command{
+    
+    CDVPluginResult* pluginResult = nil;
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+    [ForeSee incrementPageViews];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+
+-(void)incrementSignificantEventCount: (CDVInvokedUrlCommand*)command{
+
+    CDVInvokedUrlCommand* pluginResult = nil;
+
+    NSString* key = [command.arguments objectAtIndex:0];
+
+    if (key != nil && [key length] > 0 ) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        [ForeSee incrementSignificantEventCountWithKey:key];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void)resetState: (CDVInvokedUrlCommand*)command{
+    CDVPluginResult* pluginResult = nil;
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+    [ForeSee resetState];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 
 @end
