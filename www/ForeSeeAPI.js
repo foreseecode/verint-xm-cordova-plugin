ead509079cb68e956bfc1bab6a674307e4300d75
Donal Ryan <donal.ryan@verint.com>
2023-01-31 15:10:42 +0000

    remove package json import

--- www/ForeSeeAPI.js
+++ www/ForeSeeAPI.js
@@ -1,6 +1,6 @@
 var exec = require("cordova/exec");
-var pjson = require('./package.json');
-var version = pjson.version
+//var pjson = require('./package.json');
+//var version = pjson.version
 const ForeSeeAPI = "ForeSeeAPI";
 const PlatformVersionKey = "crossPlatformVersion";
 
@@ -187,7 +187,7 @@ exports.resetState = function (success, error) {
  */
 exports.start = function (success, error) {
     exec(success, error, ForeSeeAPI, "start", []);
-    exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
+    //exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
 };
 
 /**
@@ -207,7 +207,7 @@ exports.start = function (success, error) {
  */
 exports.startWithConfigurationFile = function (args, success, error) {
     exec(success, error, ForeSeeAPI, "startWithConfigurationFile", validate(args));
-    exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
+    //exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
 };
 
 /**
@@ -226,7 +226,7 @@ exports.startWithConfigurationFile = function (args, success, error) {
  */
 exports.startWithConfigurationJson = function (args, success, error) {
     exec(success, error, ForeSeeAPI, "startWithConfigurationJson", validate(args));
-    exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
+    //exec(success, error, ForeSeeAPI, "addCPPValue", [PlatformVersionKey, version]);
 };
 
 /**
